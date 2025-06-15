ff6d9b820de0fc5a70b6f024407af47a82bde355
VanessaLin9 <vanessa7591@gmail.com>
2025-06-15 19:06:58 +0800

    refactor authentication flow and improve credential handling

--- sync_gcal_to_notion.py
+++ sync_gcal_to_notion.py
@@ -1,5 +1,6 @@
 import datetime
 import os
+import sys
 import requests
 from dotenv import load_dotenv
 from google.oauth2.credentials import Credentials
@@ -12,23 +13,26 @@ NOTION_TOKEN = os.getenv("NOTION_TOKEN")
 NOTION_DATABASE_ID = os.getenv("NOTION_DATABASE_ID")
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+# Token paths
+TOKEN_PATH = os.path.join(BASE_DIR, 'token.json')
+CREDENTIALS_PATH = os.path.join(BASE_DIR, 'credentials.json')
+
 # Google Calendar API Scope
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 DATE_AHEAD = 14  # Check for events in the next 14 days
+force_login = '--force-login' in sys.argv
 
 def get_upcoming_events(check_range):
-    creds = None
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    else:
-        flow = InstalledAppFlow.from_client_secrets_file(
-            os.path.join(BASE_DIR, 'credentials.json'), SCOPES
-        )
-        creds = flow.run_console()
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
+    creds = get_credentials() 
+    # if os.path.exists(TOKEN_PATH):
+    #     creds = Credentials.from_authorized_user_file(TOKEN_PATH, SCOPES)
+    # else:
+    #     flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_PATH, SCOPES)
+    #     creds = flow.run_console()
+    #     # Save the credentials for the next run
+    #     with open(TOKEN_PATH, 'w') as token:
+    #         token.write(creds.to_json())
 
     service = build('calendar', 'v3', credentials=creds)
     now = datetime.datetime.utcnow()
@@ -46,6 +50,20 @@ def get_upcoming_events(check_range):
         print('No upcoming events found.')
         return None
     return events
+
+
+def get_credentials():
+    if force_login or not os.path.exists(TOKEN_PATH):
+        print("Login Google on the browser!")
+        flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_PATH, SCOPES)
+        creds = flow.run_local_server(port=0, open_browser=True)  # 自動打開預設瀏覽器
+        with open(TOKEN_PATH, 'w') as token:
+            token.write(creds.to_json())
+        print("✅ Login successful and token saved.")
+    else:
+        creds = Credentials.from_authorized_user_file(TOKEN_PATH, SCOPES)
+    return creds
+
 
 def check_event_exists(event_id):
     url = f"https://api.notion.com/v1/databases/{NOTION_DATABASE_ID}/query"
@@ -102,6 +120,7 @@ def create_notion_item(event):
     else:
         print("❌ Failed to add to Notion:", res.text)
 
+# Main execution
 if __name__ == '__main__':
     results = []
     event = get_upcoming_events(DATE_AHEAD)
